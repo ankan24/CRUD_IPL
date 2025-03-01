efabbf53ba242a99b97258eb4aca049cf1b00eee
Ankan Ghorai <ankanghorai30@gmail.com>
2025-03-01 13:21:14 +0530

    all options add

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const bodyParser = require('body-parser')
-const homeRoute = require('./routes/home.js') 
+
+const homeRoute = require('./routes/home.route.js') 
+const blogRoute = require('./routes/blog.route.js')
+const contactRoute = require('./routes/contact.route.js')
+const aboutRoute = require('./routes/about.route.js');
+
 const app = express();
 
 
 app.set('view engine', 'ejs');
-
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
 app.use('/',homeRoute);
+app.use('/blog', blogRoute);
+app.use('/contact',contactRoute);
+app.use('/about',aboutRoute);
 
 mongoose.connect('mongodb+srv://demouser:demo123@cluster0.ap4zayq.mongodb.net/IPL?retryWrites=true&w=majority&appName=Cluster0',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(()=>console.log('Connect to Mongoose server')
 ).catch(()=>console.log('Error connecting to Mongoose server'))
-
 
 
 app.listen(3000,(err)=>{
--- routes/home.route.js
+++ routes/home.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Club = require('../models/Club.js');
+const Club = require('../models/Club.model.js');
 
 
 router.get('/', async (req, res, next) => {
